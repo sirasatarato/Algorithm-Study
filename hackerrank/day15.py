9016c1517196172a092cd07714801efccf01ad15
sirasatarato <sirasatarato@gmail.com>
2021-04-05 14:31:40 +0900

    [HackerRank] Day 14, 15

---
+++ hackerrank/day15.py
@@ -0,0 +1,29 @@
+class Node:
+    def __init__(self,data):
+        self.data = data
+        self.next = None
+class Solution:
+    def display(self,head):
+        current = head
+        while current:
+            print(current.data,end=' ')
+            current = current.next
+
+    def insert(self,head,data):
+        node = Node(data)
+        if(head == None):
+            return node
+        else:
+            current = head
+            while current.next != None:
+                current = current.next
+            current.next = node
+            return head
+
+mylist= Solution()
+T=int(input())
+head=None
+for i in range(T):
+    data=int(input())
+    head=mylist.insert(head,data)
+mylist.display(head);
