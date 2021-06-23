89464407624d7a5a1918e680edc9500ab33c0922
sirasatarato <sirasatarato@gmail.com>
2021-06-24 08:40:58 +0900

    [HackerRank] Largest Rectangle

---
+++ hackerrank/stacks_and_queues/Largest Rectangle.py
@@ -0,0 +1,43 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+def largestRectangle(heights):
+    stack = list()
+    index = 0
+    largest_rectangle = 0
+
+    while index < len(heights):
+        if (not stack) or (heights[stack[-1]] <= heights[index]):
+            stack.append(index)
+            index += 1
+        else:
+            top_of_stack = stack.pop()
+            area = (heights[top_of_stack] *
+                    ((index - stack[-1] - 1) if stack else index))
+            largest_rectangle = max(largest_rectangle, area)
+
+    while stack:
+        top_of_stack = stack.pop()
+        area = (heights[top_of_stack] *
+                ((index - stack[-1] - 1) if stack else index))
+        largest_rectangle = max(largest_rectangle, area)
+
+    return largest_rectangle
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    h = list(map(int, input().rstrip().split()))
+
+    result = largestRectangle(h)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
